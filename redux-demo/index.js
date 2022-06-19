ff01ff41d396aae599a5ac99bc72e475484d4ac6
nikitha26 <mnikitha365@gmail.com>
2022-06-19 16:57:04 +0530

    created reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,4 +1,8 @@
-const CAKE_ORDERED = CAKE_ORDERED;
+const redux = require('redux')
+const createStore = redux.legacy_createStore
+
+
+const CAKE_ORDERED = 'CAKE_ORDERED';
 
 function orderCake(){     //Action Creator
    return{     //return Action Obj
@@ -6,3 +10,26 @@ function orderCake(){     //Action Creator
         quanity:1
     }
 }
+
+const intialState = {   //In one object
+    numofCakes:10,
+}
+
+//Reducer --> take two(state and action) arguments and return new state
+const reducer = (state = intialState,action) =>{
+  switch (action.type) {
+    case CAKE_ORDERED:
+        return {
+            numofCakes:state.numofCakes-1,
+        }
+        break;
+  
+    default:
+        return state;
+        break;
+  }
+}
+
+const store = createStore(reducer)
+console.log('IntialState', store.getState());
+
